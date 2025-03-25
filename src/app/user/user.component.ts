4c30ece028a46b71fd4147106f47110491150399
Daniele Colaci <daniele.colaci@mindthepp.it>
2025-03-25 16:03:00 +0100

    Add Users List

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+
+
+  get imagePath() {
+    return '/users/' + this.avatar;
+  }
+
+  onSelectUser() { }
+}
